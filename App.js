fde7b1851577716ccd39499c885dac3e8d4ff19b
Nicolas <ngramirez313an@gmail.com>
2026-04-09 21:57:15 -0300

    Clase cuentaBancaria creada y finalizada

---
+++ App.js
@@ -0,0 +1,18 @@
+class cuentaBancaria{
+    constructor (titular, saldo){
+        this.titular = titular,
+        this.saldo = saldo;
+    }
+
+    depositar(monto){
+        monto = this.saldo + monto;
+    }
+
+    retirar(monto){
+        monto = this.saldo - monto;
+    }
+}
+
+const miCuenta = new cuentaBancaria("Nicolas", 10000);
+//miCuenta.depositar(1000);
+console.log("Saldo actual: " + miCuenta.depositar(1000).monto)
